38354d67189718458899a9ad841e603836e08bfc
Alex74747 <alexis.dauphin74@gmail.com>
2024-12-05 05:30:48 +0100

    Commenting, formatting the import scripts.

--- csv_to_database_extensions.py
+++ csv_to_database_extensions.py
@@ -1,24 +1,27 @@
-
+# Import sqlite3 package and initialise the database cursor
 import sqlite3
 db_con = sqlite3.connect("website/db.sqlite3")
 db_cur = db_con.cursor()
 
-first=True
+first=True # Boolean variable used to check if we're on the first line of the CSV
 
+# Get all extension names
 db_res = db_cur.execute("SELECT DISTINCT extension_name FROM board_games_extensions")
+# Now make a proper array following the last request.
 extension_name_list = []
 for i in db_res.fetchall():
     extension_name_list.append(i[0])
 
+# Open Jeu_csv-Extentions.csv in read-only mode.
 with open("Jeu_csv-Extentions.csv", "r") as file:
-    for line in file:
-        if not first:
+    for line in file: # For each line
+        if not first: # Allows to forget the first line (titles) of the csv
             L = line.split(";")
             extension_name = L[0]
             game_name = L[1]
             time_add = L[2]
-            game_name_normalised = game_name
-            game_name_normalised = game_name_normalised.replace("'", "''")
+            # Change game_name in order to escape the ' symbol in SQL.
+            game_name_normalised = game_name.replace("'", "''")
 
             game_id = db_cur.execute("SELECT game_id from board_games_games where game_name='{}'".format(game_name_normalised)).fetchall()[0][0]
 
@@ -26,5 +29,5 @@ with open("Jeu_csv-Extentions.csv", "r") as file:
                 # Then add it to the database
                 db_cur.execute("""INSERT INTO board_games_extensions VALUES (NULL,?, ?, ?)""",(time_add, game_id, extension_name))
                 db_con.commit()
-        else:
+        else: # # Allows to forget the first line (titles) of the csv
             first=False

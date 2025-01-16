02077981758c67695c623d83092b2f66073d7473
Triw-12 <pagezy.tristan@gmail.com>
2025-01-16 12:13:40 +0100

    Alter add game

--- website/board_games/views.py
+++ website/board_games/views.py
@@ -5,6 +5,7 @@ from board_games.recommandation.hybride import notes
 from authentification.models import User
 from datetime import datetime
 from django.http import HttpResponseForbidden, HttpResponseNotFound
+import logging
 
 
 def home(request):
@@ -68,19 +69,34 @@ def game(request, id):
     rating = forms.RateGame()
     game = Games.objects.get(game_id=id)
     users = User.objects.all()
+    logger = logging.getLogger(__name__)
+    error = False
     message = ""
     if 'rated' in request.GET:
-        message = "Merci pour votre évaluation!"
+        message = "Merci pour votre évaluation! Vous avez donné la note de " + request.GET['rated'] + "/10"
     if request.method == 'POST':
         num_players = request.POST.get('num_players')
-        if num_players:
-            history = History.objects.create(game_id=game, date=datetime.now())
+        if num_players and num_players.isdigit() and int(num_players) >= game.min_players and int(num_players) <= game.max_players:
+            history = History.objects.create(game_id=game)
+            added_users = set()
             for i in range(int(num_players)):
                 user = request.POST.get('player_' + str(i+1))
-                if user:
-                    user_db = User.objects.get(id=user)
-                    History_players.objects.create(play_id=history, user_id=user_db)
-                    message = 'Partie enregistrée'
+                if user and user.isdigit():
+                    user = int(user)
+                    if user not in added_users:
+                        user_db = User.objects.get(id=user)
+                        History_players.objects.create(play_id=history, user_id=user_db)
+                        added_users.add(user)
+                    else:
+                        message = 'Un joueur a été ajouté plusieurs fois Merci de contacter un administrateur'
+                        logger.error('Un joueur a été ajouté plusieurs fois: user_id=%s, game_id=%s', user, game.game_id)
+                        error = True
+            if not error:
+                message = 'Partie enregistrée'
+                logger.info('Partie enregistrée: game_id=%s, players=%s', game.game_id, list(added_users))
+        else:
+            message = "Erreur dans le nombre de joueurs"
+            logger.error('Erreur dans le nombre de joueurs: game_id=%s, num_players=%s, min_players=%s, max_players=%s', game.game_id, num_players, game.min_players, game.max_players)
     return render(request, 'board_games/game.html', context={'game': game, 'simple_search': simple_search, 'users': users, 'message': message, 'form': rating})
 
 def profil(request, id):
@@ -101,12 +117,12 @@ def rate_game(request,id):
     if request.method == 'POST':
         form = forms.RateGame(request.POST)
         if form.is_valid():
-            existing_rating = Rating.objects.filter(game_id=game, user_id=request.user).first()
-            if existing_rating:
-                existing_rating.delete()
+            existing_ratings = Rating.objects.filter(game_id=game, user_id=request.user)
+            if existing_ratings.exists():
+                existing_ratings.delete()
             rating = form.save(commit=False)
             rating.game_id = game
             rating.user_id = request.user
             rating.save()
-            return redirect(f'../game/{id}?rated={rating.rating}')
-    return HttpResponseNotFound
+            return redirect(f'/game/{id}?rated={rating.rating}')
+    return HttpResponseNotFound()

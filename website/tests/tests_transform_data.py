b78539363151fec4639c52f3de0ef166fc3451a8
jason.jallabert <jason.jallabert@telecom-sudparis.eu>
2025-05-07 13:11:24 +0200

    un certain nombre de tests

---
+++ website/tests/tests_transform_data.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+
+from board_games.recommandation.transform_data import *
+
+
+class Transform_dataTests(TestCase) :
+	
+	
+	def setUp(self) :
+		self.game = Games.objects.create(game_name="Test game", game_length_min=10, game_length_max=20, min_players=2, max_players=4, min_age=10, difficulty=3)
+		Tags.objects.create(game_id=self.game, tag_id="RP")
+		Tags.objects.create(game_id=self.game, tag_id="Test false tag")
+		
+		self.user = User.objects.create_user(username="test", password="test")
+		
+		self.history = History.objects.create(game_id=self.game)
+		self.history_player = History_players.objects.create(play_id=self.history, user_id=self.user)
+		
+		
+	def test_games_to_vector(self) :
+		
+		vectest = games_to_vector()
+		
+		self.assertEqual(vectest[0][1], 20)
+		self.assertEqual(vectest[0][14],1) 
+
+	def test_number_play(self) :
+		
+		tabtest = number_play()
+
+		self.assertEqual(tabtest[0][0][0], 1)
